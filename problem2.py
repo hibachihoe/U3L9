f771273907d41f2bf29587ca10bed8767beaf2f6
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-17 11:12:38 -0500

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,12 @@
+print('-' *60)
+print('Locker Bot:')
+print()
+password = input('What is the password?')
+if password == 'Oh Boy':
+	print('Password is valid!')
+	print('Locker opening!')
+else:
+	print('Password invalid...enabling laser defense system,')
+	print('Pew Pew Pew')
+	print('Pew Pew Pew')
+print('-'*60)
---
+++ problem3.py
@@ -0,0 +1,11 @@
+print('-' *60)
+print('Alien Greeter Bot:')
+print()
+Orbit = input('What planet are you from?')
+if Orbit == 'Earth':
+	print('Nasa has left the chat...')
+else:
+	print('Dats crazy. Welcome to Earth little dudes.')
+	print('We have an assortment of resources.')
+	print('Enjoy your stay!')
+print('-'*60)
